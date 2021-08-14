e6af4e8e56f0dcc60dad67424a0284ca7947d5b7
Binzoo <86844089+Binzoo@users.noreply.github.com>
2021-08-14 18:41:24 +0200

    Create rcm.c

---
+++ rcm.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+int main(){
+    int i, k,j,colm, row,sum = 0, add = 0;
+    
+    printf("Give the number of rows and columns (remember rows and columns need to be same: ");
+    scanf("%d %d", &row, &colm);
+
+if (row == colm){
+
+    int a[row][colm];
+    int b[row][colm];
+    int c[row][colm];
+    for ( i = 0; i < row; i++)
+    {
+        for ( k = 0; k < colm; k++)
+        {
+            printf("Give the first matrix: ");
+            scanf("%d", &a[i][k]);
+        }
+        
+    }
+    for ( i = 0; i < row; i++)
+    {
+        for ( k = 0; k < colm; k++)
+        {
+            printf("Give the second matrix: ");
+            scanf("%d", &b[i][k]);
+        }
+        
+    }
+    
+    for ( j = 0; j < 3; j++)
+    {
+        
+        for ( i = 0; i < 3; i++)
+        {
+            
+            for ( k = 0; k < 3; k++)
+            {
+                sum = a[j][k] * b[k][i];
+                add = add + sum; 
+                
+            }
+            c[j][i] = add;
+
+            
+            add = 0;
+            sum = 0;
+        }
+        
+    }
+    for ( i = 0; i < 3; i++)
+    {
+        for ( k = 0; k < 3; k++)
+        {
+            printf("%d ", c[i][k]);
+        }
+        printf("\n");    
+    }
+}
+else
+{
+    printf("Error!! (Rows and columns need to be same)");
+}
+    
+    return 0;
+}
